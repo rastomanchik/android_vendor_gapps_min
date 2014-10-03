1e82a8f9b22b57479674e3f7a14c35912b6e74ca
Aleksandr <rastomanchik77@gmail.com>
2014-10-03 11:23:56 +0300

    add micro gapps

---
+++ gapps-vendor-blobs.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += \
+	vendor/gapps_min/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+	vendor/gapps_min/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+	vendor/gapps_min/system/lib/libAppDataSearch.so:system/lib/libAppDataSearch.so \
+	vendor/gapps_min/system/lib/libconscrypt_gmscore_jni.so:system/lib/libconscrypt_gmscore_jni.so \
+	vendor/gapps_min/system/lib/libgames_rtmp_jni.so:system/lib/libgames_rtmp_jni.so \
+	vendor/gapps_min/system/lib/libgcastv2_base.so:system/lib/libgcastv2_base.so \
+	vendor/gapps_min/system/lib/libgcastv2_support.so:system/lib/libgcastv2_support.so \
+	vendor/gapps_min/system/lib/libgmscore.so:system/lib/libgmscore.so \
+	vendor/gapps_min/system/lib/libgms-ocrclient.so:system/lib/libgms-ocrclient.so \
+	vendor/gapps_min/system/lib/libjgcastservice.so:system/lib/libjgcastservice.so \
+	vendor/gapps_min/system/priv-app/GoogleLoginService.apk:system/priv-app/GoogleLoginService.apk \
+	vendor/gapps_min/system/priv-app/GoogleOneTimeInitializer.apk:system/priv-app/GoogleOneTimeInitializer.apk \
+	vendor/gapps_min/system/priv-app/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework.apk \
+	vendor/gapps_min/system/priv-app/Phonesky.apk:system/priv-app/Phonesky.apk \
+	vendor/gapps_min/system/priv-app/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore.apk
+
---
+++ gapps-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/gapps_min/gapps-vendor-blobs.mk)
